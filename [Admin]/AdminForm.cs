9d19cff27324edb2a03490a105a6760a60435170
Viacheslav Melnyk <altt1m.zaskar@gmail.com>
2024-06-06 23:13:37 +0300

    ViewRepairRequests

--- [Admin]/AdminForm.cs
+++ [Admin]/AdminForm.cs
@@ -50,7 +50,38 @@ namespace WinFormsApp1
 
         private void btn_repairRequests_Click(object sender, EventArgs e)
         {
+            ViewRepairRequests(dataGridView_admin);
+        }
 
+        public void ViewRepairRequests(DataGridView dgw)
+        {
+            using (SqlConnection connection = db.GetConnection())
+            {
+                db.OpenConnection(connection);
+                string query = $@"
+                SELECT 
+                    ps.Id AS 'ID',
+                    ps.Address AS 'Адреса',
+                    ps.DateOfStart AS 'Дата початку',
+                    c.LastName AS 'Прізвище',
+                    c.FirstName AS 'Ім''я',
+                    c.PhoneNumber AS 'Номер телефону',
+                    c.Email AS 'Email'
+                FROM 
+                    ProvidedServices ps
+                JOIN 
+                    Clients c ON ps.ClientId = c.Id
+                WHERE 
+                    ps.Title LIKE 'Ремонт:%'";
+
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                DataTable dataTable = new DataTable(); // Створимо новий DataTable для збереження результатів запиту
+
+                adapter.Fill(dataTable); // Заповнимо DataTable результатами запиту
+                dgw.DataSource = dataTable; // Прив'яжемо DataTable до DataGridView
+
+                db.CloseConnection(connection);
+            }
         }
     }
 }
